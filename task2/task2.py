dd2d2bcf414bf781b4897c8dbbac454f6ddd37d7
Федор <83928502+jmkakaa@users.noreply.github.com>
2025-07-09 08:46:23 +0300

    Update task2.py

--- task2/task2.py
+++ task2/task2.py
@@ -1,45 +1,52 @@
-import os
+import sys
 from decimal import Decimal, getcontext
 
 
 getcontext().prec = 100
-value_list = []
-dot_list = []
 
 
-def read_info_about_circle(name1: str, name2: str):
-    try:
-        with open(name1, 'r', encoding="utf-8") as fdots, \
-        open(name2, "r", encoding="utf-8") as fcircle:
-            #вывод для проверки чтения файл circle x0 y0 r
-            for line in fcircle.readlines():
-                for value in line.split():
-                    value_list.append(int(value))
-            x1 = value_list[0]
-            y1 = value_list[1]
-            r = value_list[2]
-            # вывод для проверки чтения файл points
-            for line in fdots:
-                parts = line.strip().split()
-                if len(parts) == 2:
-                    x, y = Decimal(parts[0]), Decimal(parts[1])
-                    dot_list.append((x, y))
-            
-            for x, y in dot_list:
-                dist_sq = (x - x1) ** 2 + (y - y1) ** 2
-                r_sq = r ** 2
+def read_info_about_circle(circle: str):
+    """определяем окружность"""
+    with open(circle, 'r', encoding="utf-8") as fcircle:
 
-                if dist_sq == r_sq:
-                    print(0)
-                elif dist_sq < r_sq:
-                    print(1)
-                else:
-                    print(2)
-    except FileNotFoundError:
-        print("file error")
+        nums = []
+        for line in fcircle:
+            nums.extend(line.split())
+
+        x0, y0 = map(Decimal, nums[:2])
+        r = Decimal(nums[2])
+        return x0, y0, r
+
+        # вывод для проверки чтения файл points
+def read_indo_about_point(circle: str, points: str):
+    x0, y0, r = read_info_about_circle(circle)
+    r2 = r * r
+    """определяем точки"""
+    with open(points, 'r', encoding="utf-8") as fpoints:
+        for raw in fpoints:
+            nums = raw.strip().split()
+            x, y = map(Decimal, nums)
+            if len(nums) != 2:
+                continue
+            x, y = map(Decimal, nums)
+            d2 = (x - x0) ** 2 + (y - y0) ** 2
+
+            if d2 == r2:
+                print(0)
+            elif d2 < r2:
+                print(1)
+            else:
+                print(2)
+
 
 
 if __name__ == '__main__':
-    name1 = "points.txt"
-    name2 = "circle.txt"
-    read_info_about_circle(name1, name2)
+    circle_info, points_info = map(str, sys.argv[1:3])
+    try:
+        read_indo_about_point(circle_info, points_info)
+    except FileNotFoundError:
+        print("file not found")
+    except ValueError:
+        print("value error")
+
+#python task2/task2.py task2/circle.txt task2/points.txt
