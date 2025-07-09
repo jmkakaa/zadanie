f525ab550fa4fdb71ff95874ce29ba012652db62
Федор <83928502+jmkakaa@users.noreply.github.com>
2025-07-09 08:46:55 +0300

    Update task3.py

--- task3/task3.py
+++ task3/task3.py
@@ -1,39 +1,37 @@
 import json
-from imghdr import tests
+import sys
+from html.parser import incomplete
 
 
-def read_values(values: str):
-    with open(values, "r", encoding="utf-8") as fread:
-        data = json.load(fread)
+def read_json(values: str):
+    with open(values, "r", encoding="utf-8") as f:
+        data = json.load(f)
         return {str(item['id']): item['value'] for item in data['values']}
 
+def update(data, value_map):
+    if isinstance(data, dict):
+        if 'id' in data and 'value' in data and data['value'] == "":
+            id_str = str(data['id'])
+            if id_str in value_map:
+                data['value'] = value_map[id_str]
+        for i in data:
+            update(data[i], value_map)
+    elif isinstance(data, list):
+        for i in data:
+            update(i, value_map)
 
-def write_report(tests: str, report: str):
-    value_map = read_values()
-    result_lines = []
+def write_report(tests: str, reports: str, value_map):
+    with open(tests, "r", encoding="utf-8") as f:
+        data = json.load(f)
+        update(data, value_map)
 
-    with open(tests, "r", encoding="utf-8") as fread:
-        for line in fread:
-            stripped = line.strip()
-
-            if stripped.startswith('"id":'):
-                current_id = stripped.split(":")[1].strip().rstrip(",")
-                current_id = current_id.replace(" ", "").replace(",", "").replace('"', '')
-
-            if stripped.startswith('"value": ""'):
-                if current_id in value_map:
-                    value = value_map[current_id]
-                    line = f'      "value": "{value}"\n'
-
-            result_lines.append(line)
-
-    with open(report, "w", encoding="utf-8") as fwrite:
-        fwrite.writelines(result_lines)
+    with open(reports, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    tests = "tests.json"
-    values = "values.json"
-    report = "report.json"
-    read_values(values)
-    write_report(tests, report)
+    tests, values, report = sys.argv[1:4]
+    value_map = read_json(values)
+    write_report(tests, report, value_map)
+
+
